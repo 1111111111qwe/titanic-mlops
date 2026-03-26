dc92e47d5aa736a55b5374c18aa2c3a05bbdf42a
Mohamed Ahmed <1111111111qwe.com>
2026-03-26 06:56:12 +0200

    meet the threshold 0.70 case

--- check_threshold.py
+++ check_threshold.py
@@ -29,8 +29,8 @@ if not runs:
 accuracy = runs[0].data.metrics["accuracy"]
 print(f"Accuracy: {accuracy}")
 
-if accuracy < 0.80:
-    print("FAILED: accuracy is below 0.80 threshold!")
+if accuracy < 0.70:
+    print("FAILED: accuracy is below 0.70 threshold!")
     sys.exit(1)
 else:
-    print("PASSED: accuracy is above  0.80 threshold!")
+    print("PASSED: accuracy is above  0.70 threshold!")
